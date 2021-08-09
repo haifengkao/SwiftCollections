1bf7d022f04af43e47bb1cb518f4160d6bc1a638
Hai Feng Kao <haifeng@cocoaspice.in>
2021-08-10 00:47:07 +0800

    Initial commit

---
+++ Example/Shared/SwiftCollectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCollectionsApp.swift
+//  Shared
+//
+//  Created by Hai Feng Kao on 08/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCollectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
